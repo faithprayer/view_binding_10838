456f288ccb15ee6da76a2c5b7897a4feaf7cadf8
faithprayer <prayerfaith94@gmail.com>
2022-09-14 21:34:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10838/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10838
+
+object TaskList {
+    val taskList = listOf<Task>(
+    Task("Chapter 1", "bangun tidur", "kebiasaan sehari-hari untuk membuat hari makin indah"),
+    Task("Chapter 2","Nguli Valo","mencari naik rank tapi memang kampret"),
+    Task("chapter 3" ,"Ngopi", "habis rank dan stress ngopi adalah obat"),
+    Task("chapter 4","dimana mamaku?","mamku ada di rumah manado"),
+    Task("chapter 5","Ngoding nugas","ahhhhhh indahnya hiduppp iniiii")
+    )
+}
